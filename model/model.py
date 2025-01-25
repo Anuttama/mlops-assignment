700d2b36d8b77153e50d9f77b2779f75fde954fa
Anuttama Chakraborty <anuttamaeechakraborty@gmail.com>
2025-01-25 09:06:20 +0530

    correcting test file

--- model/model.py
+++ model/model.py
@@ -15,37 +15,34 @@ def load_and_preprocess_data(dataset_path):
     disease_df.dropna(axis=0, inplace=True)
     return disease_df
 
+
 def train_test_split_data(disease_df):
     # Prepare features and labels
-    X = np.asarray(disease_df[['age', 'Sex_male', 'cigsPerDay', 
-                               'totChol', 'sysBP', 'glucose']])
-    y = np.asarray(disease_df['TenYearCHD'])
-    
+    X = np.asarray(disease_df[['age', 'Sex_male', 'cigsPerDay', 'totChol', 'sysBP', 'glucose']])
+    y = np.asarray(disease_df['TenYearCHD'])    
     # Normalize features
     X = preprocessing.StandardScaler().fit(X).transform(X)
-    
     # Train-test split
     return train_test_split(X, y, test_size=0.3, random_state=4)
+
 
 def train_and_evaluate_model(X_train, X_test, y_train, y_test):
     # Train logistic regression model
     logreg = LogisticRegression()
     logreg.fit(X_train, y_train)
     y_pred = logreg.predict(X_test)
-    
     # Evaluate model
     accuracy = accuracy_score(y_test, y_pred)
     return accuracy
+
 
 if __name__ == "__main__":
     dataset_path = "data/dataset.csv"
     disease_df = load_and_preprocess_data(dataset_path)
     print(disease_df.head(), disease_df.shape)
     print(disease_df.TenYearCHD.value_counts())
-    
     X_train, X_test, y_train, y_test = train_test_split_data(disease_df)
     print('Train set:', X_train.shape, y_train.shape)
     print('Test set:', X_test.shape, y_test.shape)
-    
     accuracy = train_and_evaluate_model(X_train, X_test, y_train, y_test)
     print('Accuracy of the model is =', accuracy)
